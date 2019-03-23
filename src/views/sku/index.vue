b942373f4f716ca6a20a63337e8f59c0a31e9000
闫松林 <songlinlin1994@163.com>
2019-03-23 14:04:55 +0800

    first

---
+++ src/views/sku/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+<script>
+  import {getClass} from '@/api/class'
+
+  export default {
+    name: 'index',
+    data() {
+      return {}
+    },
+    methods: {
+      test() {
+        getClass
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
